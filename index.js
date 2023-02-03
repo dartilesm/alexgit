0a79060d651be8ae5b76b5a2f3e01ce1d6b841e3
Diego Artiles <diego45882@hotmail.com>
2023-02-02 22:38:09 -0300

    feat: improve index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 import chalk from "chalk";
 import yargsInteractive from "yargs-interactive";
 import applyCommit from "./lib/apply-commit.js";
-import getImprovedCommit from "./lib/generate-commit.js";
+import renderEditablePrompt from "./lib/editable-prompt.js";
+import getImprovedCommits from "./lib/generate-commit.js";
 import startSpinner from "./lib/spinner.js";
 
 
@@ -20,29 +21,33 @@ const { commit: initialCommit } = await yargsInteractive().usage("$0 <command> [
 
 const spinner = startSpinner()
 
-let suggestedCommit
+let suggestedCommits
 try {
-    suggestedCommit = await getImprovedCommit(initialCommit)
+    suggestedCommits = await getImprovedCommits(initialCommit)
 } catch (error) {}
 
-if (!suggestedCommit) {
+if (!suggestedCommits || suggestedCommits?.length === 0) {
     spinner.fail(chalk.red("Something went wrong! Please try again later 😔"))
     process.exit()
 }
 
-spinner.succeed(`Here you are! This is your commit: ${chalk.green(`"${suggestedCommit}"`)} `);
+spinner.text = "Oh! I got it!";
+await new Promise(resolve => setTimeout(resolve, 1000));
+spinner.stop()
 
 
 const suggestionOptions = {
     interactive: { default: true },
-    isSuggestedCommitAccepted: {
-        type: "confirm",
-        describe: `Did you like it 👀? If so, I will commit it for you!`,
+    commitSelected: {
+        type: "list",
+        describe: `Select one of the following options to edit and/or confirm:`,
+        choices: suggestedCommits,
     }
 }
 
-const { isSuggestedCommitAccepted } = await yargsInteractive().usage("$0 <command> [args]").interactive(suggestionOptions)
-
+const { commitSelected } = await yargsInteractive().usage("$0 <command> [args]").interactive(suggestionOptions)
+console.log({ commitSelected })
+const confirmedCommit = await renderEditablePrompt({ editableText: commitSelected, prompt: "Edit and confirm the commit, if you don't want to make changes press enter: " })
 
 function makeCommit(commit) {
     const { error, data } = applyCommit(commit)
@@ -54,15 +59,16 @@ ${data.commitData}
     process.exit()
 }
 
-if (isSuggestedCommitAccepted) makeCommit(suggestedCommit)
+console.log(confirmedCommit)
+// if (confirmedCommit) makeCommit(commitSelected)
 
-if (!isSuggestedCommitAccepted) {
+/* if (!commitSelected) {
     const customCommitOptions = {
         interactive: { default: true },
         customCommit: {
             type: "input",
             describe: "Ok, no problem! Feel free to adjust it to your liking (and press enter to commit) 🔧:",
-            default: suggestedCommit,
+            default: suggestedCommits,
             prompt: "always"
         }
     }
@@ -74,4 +80,4 @@ if (!isSuggestedCommitAccepted) {
     } finally {
         process.exit()
     }
-}
+} */
