9354659fc8246277776c7f4a16c9caff46b6cca6
Diego Artiles <diego45882@hotmail.com>
2023-02-17 00:35:00 -0300

    feat: ask for api key if needed

--- src/run-alex-git.js
+++ src/run-alex-git.js
@@ -7,41 +7,66 @@ import startSpinner from "../src/spinner.js";
 import getInitialArguments from "../src/utils/arguments.js";
 import sleep from "../src/utils/sleep.js";
 import renderVerticalLine from "../src/utils/vertical-seperator.js";
+import getAPIKey, { askForApiKey } from "./api-key.js";
 
 async function runAlexGit() {
-    const { commit: commitArg } = getInitialArguments();
+  const APIKey = await getAPIKey();
 
-    const alexgit = new Alexgit();
-    
-    await alexgit.say(commitArg ? messages.initialCommit : messages.welcome)
-    await sleep(250)
-    
-    const initialCommit = await renderPrompt("initialCommit", { initial: commitArg || "", skip: !!commitArg});
-    
-    if (commitArg) {
-        await alexgit.say(messages.initialCommit)
-    };  
-    renderVerticalLine()
-    const spinner = startSpinner(categorizeMessage({ type: "info", message: !commitArg ? "Wonderful, I got your commit message!" : "Working on it!"}))
-    
-    const suggestedCommit = await getImprovedCommits(initialCommit);
-    if (suggestedCommit.error || suggestedCommit.data.length === 0) {
-        spinner.stop()
-        console.log(`✖ ${categorizeMessage({ type: "error", message: messages.error[0] })}`)
-        process.exit()
-    }
-    
-    spinner.text = categorizeMessage({ type: "success", message: "Oh! I got it!" });
-    await sleep(1000)
-    spinner.stop()
-    
-    const commitSelected = await renderPrompt("commitList", { choices: suggestedCommit.data })
-    
-    const commitConfirmed = await renderPrompt("confirmCommit", { initial: commitSelected })
-    
-    
-    renderVerticalLine()
-    if (commitConfirmed) makeCommit(commitConfirmed)
+  const { commit: commitArg } = getInitialArguments();
+
+  const alexgit = new Alexgit();
+
+  function getAlexgitInitialMessage() {
+    if (commitArg && !APIKey) return [...messages.initialCommit, ...messages.apiKeyNotFound];
+
+    if (commitArg && APIKey) return messages.initialCommit;
+
+    if (!commitArg && !APIKey) return [...messages.apiKeyNotFound];
+
+    if (!commitArg && APIKey) return messages.welcome;
+  }
+
+  await alexgit.say(getAlexgitInitialMessage());
+  await sleep(250);
+
+  if (!APIKey) {
+    await askForApiKey();
+    renderVerticalLine();
+  }
+
+  const initialCommit = await renderPrompt("initialCommit", {
+    initial: commitArg || "",
+    skip: !!commitArg,
+  });
+
+  if (commitArg) {
+    await alexgit.say(messages.initialCommit);
+  }
+  renderVerticalLine();
+  const spinner = startSpinner(
+    categorizeMessage({
+      type: "info",
+      message: !commitArg ? "Wonderful, I got your commit message!" : "Working on it!",
+    })
+  );
+
+  const suggestedCommit = await getImprovedCommits(initialCommit);
+  if (suggestedCommit.error || suggestedCommit.data.length === 0) {
+    spinner.stop();
+    console.log(`✖ ${categorizeMessage({ type: "error", message: messages.error[0] })}`);
+    process.exit();
+  }
+
+  spinner.text = categorizeMessage({ type: "success", message: "Oh! I got it!" });
+  await sleep(1000);
+  spinner.stop();
+
+  const commitSelected = await renderPrompt("commitList", { choices: suggestedCommit.data });
+
+  const commitConfirmed = await renderPrompt("confirmCommit", { initial: commitSelected });
+
+  renderVerticalLine();
+  if (commitConfirmed) makeCommit(commitConfirmed);
 }
 
 export default runAlexGit;
