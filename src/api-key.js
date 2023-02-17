3bce0629b0c9dd31bee394b1aaeec695d193a1d1
Diego Artiles <diego45882@hotmail.com>
2023-02-17 00:33:38 -0300

    feat: add api-key file to get/set apikey

---
+++ src/api-key.js
@@ -0,0 +1,37 @@
+import fs from "fs";
+import renderPrompt from "./propmts.js";
+
+function getFormatedEnvVars(envVar) {
+  let [name, value] = envVar.split("=");
+
+  name = name.toLowerCase();
+
+  // convert snake case to camel case
+  name = name.replace(/_([a-z])/g, (g) => g[1].toUpperCase());
+
+  return { [name]: value };
+}
+
+async function getAPIKey() {
+  const currentWorkingDirectory = process.cwd();
+  // read a file from the current working directory
+  const data = fs.readFileSync(`${currentWorkingDirectory}/.alexgit`, "utf8");
+
+  const config = data.split("\n").reduce(
+    (acc, curr) => ({
+      ...acc,
+      ...getFormatedEnvVars(curr),
+    }),
+    {}
+  );
+  
+  return config.cohereApiKey;
+}
+
+export async function askForApiKey(){
+    const apiKey = await renderPrompt("askForAPIKey")
+    const currentWorkingDirectory = process.cwd();
+    fs.writeFileSync(`${currentWorkingDirectory}/.alexgit`, `COHERE_API_KEY=${apiKey}`, "utf8");
+}
+
+export default getAPIKey;
